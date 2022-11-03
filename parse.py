8ec8e92a58364517c69d26353c17abb6b55d5c10
Aimarzhan177 <marzhandzh78@gmail.com>
2022-11-03 10:50:17 +0600

    from 4week

--- parse.py
+++ parse.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import csv
 
 URL = "https://www.kivano.kg/noutbuki?brands=acer-apple"
 HEADERS = {
@@ -8,6 +9,7 @@ HEADERS = {
 }
 LIN = "https://www.kivano.kg"
 # responce = requests.get(URL, headers=HEADERS)
+CSV_FILE = "laptop.csv"
 
 
 
@@ -29,13 +31,25 @@ def get_content_from_html(html_text):
                 "image": LIN + item.find("img").get("src"),
             }
         )
-    print(laptops)
+    return laptops
+def save_data(laptops: list)->None:
+    with open(CSV_FILE, "w") as file:
+        writer = csv.writer(file,delimiter = ",")
+        writer.writerow(["Название", "Описание", "Цена", "Картинка"])
+        for laptop in laptops:
+            writer.writerow([laptop["title"], laptop["discription"],
+                             laptop["price"], laptop["image"]])
 
 
 def get_result_parse():
     html = get_html(URL, HEADERS)
     if html.status_code == 200:
         get_content_from_html(html.text)
+        laptops = get_content_from_html(html.text)
+        save_data(laptops)
+        return laptops
+
+    print(get_result_parse())
 
 
 get_result_parse()
